1b82afc0bec2f1ee9de352a7bfdad1ed2e732457
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-11-09 23:15:53 +0530

    Create LinkedList Cycle.cpp

---
+++ Easy Problems/LinkedList Cycle.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+         if(head==NULL) 
+             return false;
+         
+        ListNode *fast=head;
+        ListNode *slow=head;
+        
+        while(fast != NULL && fast->next != NULL){
+             fast=fast->next->next;
+             slow=slow->next;
+            
+             if(slow==fast)
+                return true;
+            }
+            
+        return false;
+    }
+};
