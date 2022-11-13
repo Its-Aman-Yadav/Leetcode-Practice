103c94129a39f9d4c45c5cce180acb4e40d6317a
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-11-13 19:47:16 +0530

    Create 028 Power of Two.cpp

---
+++ Easy Problems/028 Power of Two.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/power-of-two/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+         if(n==0) return 0;
+        
+         while(n%2==0)
+             n /=2;
+        
+         return n==1;
+    }
+};
