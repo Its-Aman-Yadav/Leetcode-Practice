5e30cf5f219dcb4e8e5bcd1b94d46b9b1956aecc
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-12-05 23:26:10 +0530

    Create 031 Ugly Number.cpp

---
+++ Easy Problems/031 Ugly Number.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/ugly-number/
+
+class Solution {
+public:
+    bool isUgly(int n) {
+    if(n<=0) return false;
+        
+    while(n>1){
+        if(n%2==0) n = n/2;
+        else if(n%3==0) n=n/3;
+        else if(n%5==0) n=n/5;
+        else break;
+    }
+    
+    return n==1;
+    }
+};
