17011ec513ddc9e3ec84c19e3ef12271384f540c
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-01-28 13:43:15 +0530

    Update 002 Valid Parenthesis.cpp

--- Easy Problems/002 Valid Parenthesis.cpp
+++ Easy Problems/002 Valid Parenthesis.cpp
@@ -3,6 +3,35 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack<char>st;
+        if (s.length() < 2) 
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.at(i);
+            if (c == '{' || c == '(' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty())
+                    return false;
+                if (c == '}' && st.top() == '{')
+                    st.pop();
+                else if (c == ')' && st.top() == '(')
+                    st.pop();
+                else if (c == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+public:
+    bool isValid(string s) {
         stack<char> st; 
         for(auto i:s) {  //iterate every element of s
             
