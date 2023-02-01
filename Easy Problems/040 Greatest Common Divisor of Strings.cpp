6a21ab728b748cf7a229a36fe7d9d2eadf8bc483
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-02-01 23:51:49 +0530

    1 Feb 2023

---
+++ Easy Problems/040 Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 == str2 + str1){
+            return str1.substr(0, gcd(str1.length(), str2.length()));
+        }
+        return "";
+    }
+};
