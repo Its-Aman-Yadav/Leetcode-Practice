e72a7b5efe9e0949917fb7ecd92d31ea959574c7
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-02-01 23:37:27 +0530

    Create 039 Diameter of Binary Tree.cpp

---
+++ Easy Problems/039 Diameter of Binary Tree.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL) return 0;
+        int ans = max(height(root->left),height(root->right)) +1;
+        return ans;
+    }
+   
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL) return 0;
+        int left = diameterOfBinaryTree(root->left);
+        int right = diameterOfBinaryTree(root->right);
+        int sum_of_height= height(root->left) + height(root->right) +1;
+
+        int ans  = max(sum_of_height,max(left,right));
+        return ans-1;
+    }
+};
