fa51eae3c3068041b3c555c072dabc88c90777fd
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-02-04 11:42:34 +0530

    Create Search in Rotated Sorted Array.cpp

---
+++ Medium Problems/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0; i<nums.size(); i++){
+          if(nums[i] == target)
+             return i;
+        }
+        return -1;
+    }
+};
