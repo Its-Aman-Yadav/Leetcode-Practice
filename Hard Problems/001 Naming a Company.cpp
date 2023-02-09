d5a6e17416b8788b4e0328920f8ab4a4b009a43d
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-02-09 12:26:48 +0530

    9 Feb 23

---
+++ Hard Problems/001 Naming a Company.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/naming-a-company/description/
+
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        long long ans = 0;
+        vector<unordered_set<string>> arr(26);
+        for (string s : ideas) 
+            arr[s[0] - 'a'].insert(s.substr(1));
+        
+        for (int i = 0; i < 25; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                unordered_set<string> set;
+                set.insert(arr[i].begin(), arr[i].end());
+                set.insert(arr[j].begin(), arr[j].end());
+                ans += (arr[i].size() - set.size()) * (arr[j].size() - set.size());
+            }
+        }
+        return ans * 2;
+    }
+};
