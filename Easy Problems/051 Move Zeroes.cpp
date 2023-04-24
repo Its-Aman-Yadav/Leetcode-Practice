4e03b85fcbba028fea20a4723277df06ccf0ae6b
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-04-24 05:53:33 +0530

    Create 051 Move Zeroes.cpp

---
+++ Easy Problems/051 Move Zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int lastNonZeroFoundAt = 0; 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[lastNonZeroFoundAt]);
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+};
