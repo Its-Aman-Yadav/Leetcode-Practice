ff572a99c205d7d986adb9398f09c3ab1c93369a
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-06-08 20:39:51 +0530

    Update 034 First Bad Version.cpp

--- Easy Problems/034 First Bad Version.cpp
+++ Easy Problems/034 First Bad Version.cpp
@@ -1,21 +1,21 @@
 //https://leetcode.com/problems/first-bad-version/description/
 
-class Solution{
-  public:
-    int firstBadVersion(int n){
-      long long l=1,h=n,ans;
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left=0; 
+        int right = n;
+        int ans=0;
 
-      while(l<=h){
-        long long m=(l+h)/2;
-        if(isBadVersion(m)==true){
-          h=m-1;
-          ans=m;
+        while(left<=right){
+          long long mid = left + (right-left)/2;
+          if(isBadVersion(mid)==true){
+            right = mid-1;
+            ans = mid;
+          }
+
+          else left = mid+1;
         }
-
-        else
-            l=m+1;
-      }
-
       return ans;
     }
 };
