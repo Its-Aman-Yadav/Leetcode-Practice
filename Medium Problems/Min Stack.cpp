7e119ac5045acdfca0526746a4dc8d6a7b5cdb00
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-06-17 11:36:52 +0530

    Create Min Stack.cpp

---
+++ Medium Problems/Min Stack.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/min-stack/description/
+
+class MinStack {
+public:
+    vector< pair<int,int> > s;
+	
+    MinStack() { }
+    
+    void push(int val) {
+        if(s.empty())
+            s.push_back({val,val});
+        else
+            s.push_back({val,min(s.back().second,val)});    
+    }
+    
+    void pop() { s.pop_back(); }
+    
+    int top() { return s.back().first; }
+    
+    int getMin() { return s.back().second; }
+};
