d1d62712e9284b8389182b37230d439f685244f2
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2024-01-13 23:52:14 +0530

    Create Minimum Number of Steps to Make Two Strings Anagram.cpp

---
+++ Medium Problems/Minimum Number of Steps to Make Two Strings Anagram.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/description/
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        vector<int> count_s(26, 0);
+        vector<int> count_t(26, 0);
+
+        for (char ch : s) {
+            count_s[ch - 'a']++;
+        }
+
+        for (char ch : t) {
+            count_t[ch - 'a']++;
+        }
+
+        int steps = 0;
+        for (int i = 0; i < 26; i++) {
+            steps += abs(count_s[i] - count_t[i]);
+        }
+
+        return steps / 2;  
+    }
+};
