e87d90d27bfc487da0107630e0dbe03c6f8455d5
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2024-01-17 12:48:06 +0530

    Create Insert Delete GetRandom O(1).cpp

---
+++ Medium Problems/Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,50 @@
+//https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+
+class RandomizedSet {
+    vector<int> v;
+    unordered_map<int,int> mp;
+public:
+   
+    RandomizedSet() {
+    }
+
+    bool search(int val){
+
+         if(mp.find(val)!=mp.end())
+            return true;
+         return false;
+
+    }
+
+    
+    bool insert(int val) {
+
+        if(search(val))
+            return false;
+
+        v.push_back(val);
+        mp[val] = v.size()-1;
+        return true;
+    }
+
+    
+    bool remove(int val) {
+
+        if(!search(val))
+            return false;
+
+       
+        auto it = mp.find(val);
+        v[it->second] = v.back();
+        v.pop_back();
+        mp[v[it->second]] = it->second;
+        mp.erase(val);
+        return true;
+    }
+
+   
+    int getRandom() {
+
+        return v[rand()%v.size()];
+    }
+};
