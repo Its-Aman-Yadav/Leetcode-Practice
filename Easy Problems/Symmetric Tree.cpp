3e1b82b424d3b370d2fb1e72e85b0872c4b73147
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2024-03-28 11:38:08 +0530

    Create Symmetric Tree.cpp

---
+++ Easy Problems/Symmetric Tree.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/symmetric-tree/description/
+
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return (left->val == right->val) && isMirror(left->left, right->right) && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
+
+};
