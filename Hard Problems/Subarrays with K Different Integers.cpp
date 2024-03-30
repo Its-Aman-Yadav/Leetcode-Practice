860d7b8f925dc47c83fd92a2079431513e816b50
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2024-03-30 20:58:44 +0530

    Create Subarrays with K Different Integers.cpp

---
+++ Hard Problems/Subarrays with K Different Integers.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/subarrays-with-k-different-integers/description/?envType=daily-question&envId=2024-03-30
+
+
+class Solution {
+public:
+    
+    int countSubarraysWithAtMostKDistinct(vector<int>& nums, int k){
+        int n=nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        int i=0, j=0;
+        
+        int c=0;
+        
+        while(j<n){
+            mp[nums[j]]++;
+            
+            while(i<=j && mp.size()>k){
+                if(--mp[nums[i]] == 0) mp.erase(nums[i]);
+                i++;
+            }
+            
+            c += (j-i+1);
+            
+            j++;
+        }
+        
+        return c;
+    }
+    
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return countSubarraysWithAtMostKDistinct(nums,k)-countSubarraysWithAtMostKDistinct(nums,k-1);
+    }
+};
